4a591336fbe9d4334257fd0b7e2778addb269229
Toyosi Olabisi <toyosio@gmail.com>
2017-11-16 19:19:14 -0600

    still workin

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,13 +1,13 @@
 var eighties = ["80s movies", "80s fashion", "80s shows", "80s music", "80s hair", "alfie", "80s electronics", "80s commercials", "80s art", "80s comedy", "80s dance", "80s toys"]
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=hAzUG1kNyCQotsbFDUbozNHAA3eGGibO&q=80s&limit=10&offset=0&rating=PG-13&lang=en";
+//var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=hAzUG1kNyCQotsbFDUbozNHAA3eGGibO&q=80s&limit=10&offset=0&rating=PG-13&lang=en";
 
 // Create buttons
 function toyFunction() {
     $("#decadebutton").empty();
     for (var i = 0; i < eighties.length; i++) {
         var e = $("<button>");
-
+       // console.log(eighties[i]);
         e.addClass("btn btn-dark");
         e.attr("data-name", eighties[i]);
         e.text(eighties[i]);
@@ -22,12 +22,18 @@ $("#addone").on("click", function (event) {
     eighties.push(one);
     console.log(one);
     toyFunction();
-    queryNewKeyword();
+})
+
+//give button a value
+
+$("#decadebutton").on("click", function (event) {
+    var gifValue = $(this).val("data-name", eighties[i]);
+    console.log(gifValue);
 })
 
 //Give button it's data state (Attribute still and animated state to each button??)
-$(".button").on("click", function (event) {
-    var gifValue = $(this).attr("data-state");
+$("#decadebutton").on("click", function (event) {
+    var gifState = $(this).attr("data-state");
     if ("state === still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
@@ -39,10 +45,10 @@ $(".button").on("click", function (event) {
 
 
 //Search key word when button is clicked
-function queryNewKeyword() {
-    $(".btn").on("click", function () {
+/*function queryNewKeyword() {
+    $("#decadebutton").on("click", function () {
         $("#eighties").html(" ");
-        queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).text()+"&api_key=hAzUG1kNyCQotsbFDUbozNHAA3eGGibO&q=80s&limit=10&offset=0&rating=PG-13&lang=en";
+        queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).text() + "&api_key=hAzUG1kNyCQotsbFDUbozNHAA3eGGibO&q=80s&limit=10&offset=0&rating=PG-13&lang=en";
 
         $.ajax({
 
@@ -50,15 +56,16 @@ function queryNewKeyword() {
             method: "GET"
         }).done(function(response) {
             for (var i = 0; i < response.data.length; i+=10) {
-                $("#eighties").append('<p>Rating: '+response.data[i].rating+'</p><br><img src="'+response.data[i].images.fixed_height_still.url+'"data-still="'+response.data[i].images.fixed_height_still.url+'" data-animate="'+response.data[i].images.fixed_height.url+'" data-state="still"class="gif"onclick="'+clickOnImgs()+'">');
+                $("#eighties").append('<p>Rating: ' + response.data[i].rating + '</p><br><img src="' + response.data[i].images.fixed_height_still.url + '"data-still="' + response.data[i].images.fixed_height_still.url + '" data-animate="' + response.data[i].images.fixed_height.url + '" data-state="still"class="gif"onclick="' + clickOnImgs() + '">');
+                console.log(response);
             };
         });
     });
-}
+}*/
 
 
 $(document).ready(function () {
 
     toyFunction();
-    queryNewKeyword();
+   // queryNewKeyword();
 })
