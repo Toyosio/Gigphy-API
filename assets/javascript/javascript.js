9263326d80565726f8055cd455f08bd0d7481117
Toyosi Olabisi <toyosio@gmail.com>
2017-11-18 17:09:06 -0600

    fixed functions

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,6 +1,6 @@
 var eighties = ["80s movies", "80s fashion", "80s shows", "80s music", "80s hair", "alfie", "80s electronics", "80s commercials", "80s art", "80s comedy", "80s dance", "80s toys"];
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=hAzUG1kNyCQotsbFDUbozNHAA3eGGibO&q=80s&limit=10&offset=0&rating=PG-13&lang=en";
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=hAzUG1kNyCQotsbFDUbozNHAA3eGGibO&q=80s&limit=10";
 
 //Create buttons
 function toyFunction() {
@@ -22,16 +22,17 @@ $("#addone").on("click", function (event) {
     toyFunction();
 });
 
+
 //give button a value; When button is clicked, Gif appears
 $(document).on("click", "button", function () {
     var gifValue = $(this).attr("data-name");
     console.log(gifValue);
     $("#eighties").html(" ");
-    queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifValue + "&api_key=hAzUG1kNyCQotsbFDUbozNHAA3eGGibO&q=80s&limit=10&offset=0&rating=PG-13&lang=en";
+    queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifValue + "&api_key=hAzUG1kNyCQotsbFDUbozNHAA3eGGibO&q=80s&limit=10";
 
     $.ajax({
         url: queryURL,
-        method: 'GET'
+        method: "GET"
     }).done(function (response) {
         for (var i = 0; i < response.data.length; i += 10) {
             $("#eighties").append('<p>Rating: ' + response.data[i].rating + '</p><br><img src="' + response.data[i].images.fixed_height_still.url + '"data-still="' + response.data[i].images.fixed_height_still.url + '" data-animate="' + response.data[i].images.fixed_height.url + '"data-state="still"class="gif"onclick="' + clickOnImgs() + '">');
@@ -41,16 +42,18 @@ $(document).on("click", "button", function () {
 });
 
 //Give button it's data state (Attribute still and animated state to each button??)
-$("button").on("click", function (event) {
-    var gifState = $(this).attr("data-state");
-    if ("state === still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-    } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-    }
-});
+function clickOnImgs() {
+    $("#eighties").on("click", function (event) {
+        var gifState = $(this).attr("data-state");
+        if ("state === still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
+}
 
 $(document).ready(function () {
 
